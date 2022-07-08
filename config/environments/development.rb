2e82308234fd3999861de946c79e30d03b733627
marcosbsa <marcos.bsa@hotmail.com>
2022-07-08 01:07:55 -0300

    deploy7

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,8 +65,8 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.deliveries = []
-  config.action_mailer.default_url_options = { host: 'https://property-finder22.herokuapp.com/blog' }
+  # config.action_mailer.deliveries = []
+  # config.action_mailer.default_url_options = { host: 'https://property-finder22.herokuapp.com/blog' }
 
 
 
