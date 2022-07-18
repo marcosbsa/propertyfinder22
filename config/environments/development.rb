b74cdd9f9efb4b7782e02b10221ef8dda6d924bc
marcosbsa <marcos.bsa@hotmail.com>
2022-07-18 19:04:10 -0300

    deploy39

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -76,9 +76,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: 'gmail.com',
-    user_name: Rails.application.credentials.gmail[:user_name],
-    password: Rails.application.credentials.gmail[:password],
+    domain: 'property-finder22.herokuapp.com',
+    user_name: 'marcosbsa@gmail.com',
+    password: 'lulxdkxnhakunewu',
     authentication: 'plain',
     enable_starttls_auto: true
     }
