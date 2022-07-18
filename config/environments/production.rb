0142e777a3293e9baf8ab8ea9607bab023e5890b
marcosbsa <marcos.bsa@hotmail.com>
2022-07-18 19:37:26 -0300

    deploy40

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -124,9 +124,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: 'gmail.com',
-    user_name: Rails.application.credentials.gmail[:user_name],
-    password: Rails.application.credentials.gmail[:password],
+    domain: 'property-finder22.herokuapp.com',
+    user_name: 'marcosbsa@gmail.com',
+    password: 'lulxdkxnhakunewu',
     authentication: 'plain',
     enable_starttls_auto: true
     }
